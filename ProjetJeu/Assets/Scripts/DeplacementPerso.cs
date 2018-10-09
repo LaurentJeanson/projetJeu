06b1d805347db3b0e550197123401fd65c64b064
Phil <philippe.thibeault99@outlook.com>
2018-10-09 17:32:54 -0400

    Personnage
    Début du personnage et organisation du projet

---
+++ ProjetJeu/Assets/Scripts/DeplacementPerso.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementPerso : MonoBehaviour {
+
+    public float vitesse;
+
+    private Rigidbody rb;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float deplaceHorizontal = Input.GetAxis("Horizontal");
+        float deplaceVertical = Input.GetAxis("Vertical");
+        float mouseInput = Input.GetAxis("Mouse X");
+
+        Vector3 deplacement = new Vector3(deplaceHorizontal, 0f, deplaceVertical);
+        Vector3 direction = new Vector3(0, mouseInput, 0);
+
+        rb.AddForce(deplacement * vitesse);
+        transform.Rotate(direction);
+
+    }
+}

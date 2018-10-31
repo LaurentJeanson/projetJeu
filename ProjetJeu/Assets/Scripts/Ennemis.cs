b07e30af1e99a5de1fc8c72ef009f5ad45005234
Carolanne <myridya@hotmail.com>
2018-10-31 12:55:00 -0400

    modification animations ennemi (Bleu)
    Ajustement de l'animations permettant son bon fonctionnement

--- ProjetJeu/Assets/Scripts/Ennemis.cs
+++ ProjetJeu/Assets/Scripts/Ennemis.cs
@@ -7,16 +7,20 @@ public class Ennemis : MonoBehaviour {
 
     public GameObject laCible;
     NavMeshAgent navAgent;
+    Animator ennemiAnim;
 
 	// Use this for initialization
 	void Start () {
         navAgent = GetComponent<NavMeshAgent>();
+        ennemiAnim = GetComponent<Animator>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         navAgent.SetDestination(laCible.transform.position);
+        print(navAgent.velocity.magnitude);
+        ennemiAnim.SetFloat("vitesse", navAgent.velocity.magnitude);
 		
 	}
 }

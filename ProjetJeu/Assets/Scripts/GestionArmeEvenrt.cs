69ec71257c870ba1a179c98123f9f1ca69d161fb
devjeu <43211608+devjeu@users.noreply.github.com>
2018-11-14 12:50:54 -0500

    Changement titre

---
+++ ProjetJeu/Assets/Scripts/GestionArmeEvenrt.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionArmeEvenrt : MonoBehaviour {
+    public GameObject Texte;
+    public GameObject Panel;
+    public GameObject Texte2;
+    public GameObject Panel2;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Texte.SetActive(false);
+            Panel.SetActive(false);
+            Texte2.SetActive(true);
+            Panel2.SetActive(true);
+            StartCoroutine(Cerveautexte());
+        }
+    }
+    IEnumerator Cerveautexte()
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(10);
+        print(Time.time);
+        Texte2.SetActive(false);
+        Panel2.SetActive(false);
+    }
+}
---
+++ ProjetJeu/Assets/Scripts/ScriptPrendreArme.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptPrendreArme : MonoBehaviour {
+    public GameObject FusilDeco;
+    public GameObject FusilPerso;
+    public GameObject particules;
+    public GameObject texte;
+    public GameObject panel;
+    public GameObject texte2;
+    public GameObject panel2;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            particules.SetActive(false);
+            FusilDeco.SetActive(false);
+            FusilPerso.SetActive(true);
+            texte.SetActive(false);
+            panel.SetActive(false);
+            texte2.SetActive(true);
+            panel2.SetActive(true);
+        }
+    }
+}

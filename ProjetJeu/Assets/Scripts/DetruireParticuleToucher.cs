0284e4cb78f11c9b8809bf6e93674b7edf1df6c8
Phil <philippe.thibeault99@outlook.com>
2018-12-07 19:01:07 -0500

    Testing

--- ProjetJeu/Assets/Scripts/DetruireParticuleToucher.cs
+++ ProjetJeu/Assets/Scripts/DetruireParticuleToucher.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DetruireParticuleTouche : MonoBehaviour {
+public class DetruireParticuleToucher : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
